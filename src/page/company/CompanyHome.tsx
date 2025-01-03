45cba1c6934184d7954897a92a56f44992627a9e
Reggis94 <regisboamah@gmail.com>
2025-01-03 21:26:31 +0100

    Ongoing CompanyHome page beginning services display

--- src/page/company/CompanyHome.tsx
+++ src/page/company/CompanyHome.tsx
@@ -1,11 +1,13 @@
 import { useParams } from "react-router-dom";
 import { useState } from "react";
+import "./CompanyHome.css";
 
 function CompanyHome() {
   const { id } = useParams();
   const [contentServicesBookingDisplayer, setContentServicesBookingDisplayer] =
     useState("");
-
+  const [isServicesListVisible, setIsServicesListVisible] = useState(false);
+  const [isServicesLoading, setIsServicesLoading] = useState(false);
   function displayServices(id: string) {
     setContentServicesBookingDisplayer("Loading");
     setTimeout(function () {
@@ -15,13 +17,44 @@ function CompanyHome() {
     }, 2000);
   }
 
+  function toggleServicesVisibility() {
+    setIsServicesListVisible((prev) => !prev);
+    fetchServices();
+    alert(isServicesListVisible);
+  }
+
+  function fetchServices() {}
+
   return (
     <>
       {id}
-      <div className="services-booking-displayer">
-        {contentServicesBookingDisplayer}
+      <div className="wrapper" style={{ background: "red", height: "1000px" }}>
+        HELLO
+        <div className="services-booking-displayer">
+          {contentServicesBookingDisplayer}
+        </div>
       </div>
-      <button onClick={() => displayServices(id)}>BOOK NOW</button>{" "}
+      <div className="services-footer">
+        <button className="services-button" onClick={toggleServicesVisibility}>
+          SEE SERVICES AND BOOK NOW
+        </button>
+        <div
+          className={`services-lists ${
+            isServicesListVisible ? "visible" : "hidden"
+          }`}
+        >
+          <div>
+            <h1>SERVICE 1</h1>
+            <p>Azerty az azerty az Azerty az</p>
+            <img src=""></img>
+          </div>
+          <div>
+            <h1>SERVICE 1</h1>
+            <p>Azerty az azerty az Azerty az</p>
+            <img src=""></img>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
